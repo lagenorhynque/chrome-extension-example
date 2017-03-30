7c4360675f44f0e7f927620c9e916b3ded0325ad
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-03-30 17:47:08 +0900

    use jayq for DOM manipulation

--- src/cljs/chrome_extension_example/core.cljs
+++ src/cljs/chrome_extension_example/core.cljs
@@ -1,12 +1,12 @@
-(ns chrome-extension-example.core)
-  ; (:require [jayq.core :refer [$ html]]))
+(ns chrome-extension-example.core
+  (:require [jayq.core :refer [$ html]]))
 
 (defn handler []
   (.getSelected (.-tabs js/chrome) nil (fn [tab]
-                                         (set! (.-innerHTML (.getElementById js/document "title"))
-                                               (.-title tab))
-                                         (set! (.-innerHTML (.getElementById js/document "url"))
-                                               (.-url tab)))))
+                                         (.text ($ "#title")
+                                                (.-title tab))
+                                         (.text ($ "#url")
+                                                (.-url tab)))))
 
-(let [b (.getElementById js/document "button")]
-  (.addEventListener b "click" handler false))
+(let [b ($ "#button")]
+  (.on b "click" handler))
